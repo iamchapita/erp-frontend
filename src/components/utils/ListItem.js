42533ae9038092841d2ab20c1e5e90e6887d9c0f
iamchapita <alejandrom646@gmail.com>
2023-07-20 12:29:26 -0600

    Se eliminó impresión en consola

--- src/components/utils/ListItem.js
+++ src/components/utils/ListItem.js
@@ -1,37 +1,36 @@
-import { IconButton, Tooltip } from '@mui/material'
-import React from 'react'
-import { Link } from 'react-router-dom';
+import { IconButton, Tooltip } from "@mui/material";
+import React from "react";
+import { Link } from "react-router-dom";
 
 export const ListItem = ({ Icon, Title, ClassName, path }) => {
-    console.log(path);
-    return (
-        <Link to={path} className={`text-center transition ${ClassName}`}>
-            <Tooltip
-                componentsProps={
-                    {
-                        tooltip: {
-                            sx: {
-                                backgroundColor: '#fff',
-                                color: '#000000',
-                                fontSize: '0.8rem',
-                                border: '1px solid rgba(79, 209, 197, 0.15)'
-                            }
-                        },
-                        arrow: {
-                            sx: {
-                                color: '#fff',
-                                border: '1px solid rgba(79, 209, 197, 0.15)'
-                            }
-                        }
-                    }
-                }
-                arrow
-
-                title={Title} placement='right' followCursor>
-                <IconButton>
-                    <Icon fontSize='large' className='text-white' />
-                </IconButton>
-            </Tooltip>
-        </Link>
-    )
-}
+	return (
+		<Link to={path} className={`text-center transition ${ClassName}`}>
+			<Tooltip
+				componentsProps={{
+					tooltip: {
+						sx: {
+							backgroundColor: "#fff",
+							color: "#000000",
+							fontSize: "0.8rem",
+							border: "1px solid rgba(79, 209, 197, 0.15)",
+						},
+					},
+					arrow: {
+						sx: {
+							color: "#fff",
+							border: "1px solid rgba(79, 209, 197, 0.15)",
+						},
+					},
+				}}
+				arrow
+				title={Title}
+				placement="right"
+				followCursor
+			>
+				<IconButton>
+					<Icon fontSize="large" className="text-white" />
+				</IconButton>
+			</Tooltip>
+		</Link>
+	);
+};

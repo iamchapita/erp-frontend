19557839d06921bfc6ee24aac112929f7adc3e3b
iamchapita <alejandrom646@gmail.com>
2023-07-20 14:13:19 -0600

    Implemnetacion de funcion para hacer post de data

--- src/components/utils/fetch.js
+++ src/components/utils/fetch.js
@@ -11,3 +11,20 @@ export const FetchData = async (url, authToken) => {
 		console.error("Error al realizar la solicitud:", error);
 	}
 };
+
+export const PostData = async (url, authToken = null, body) => {
+	try {
+		const response = await fetch(`http://localhost:8082/${url}`, {
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json",
+				Authorization: authToken,
+			},
+			body: JSON.stringify(body),
+		});
+		const data = await response.json();
+		return data; // Hacer algo con los datos obtenidos
+	} catch (error) {
+		console.error("Error al realizar la solicitud:", error);
+	}
+};

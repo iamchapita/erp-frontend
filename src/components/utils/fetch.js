bd4736d1ecfe7c6cc26c39fcaf33bfff5f822a28
iamchapita <alejandrom646@gmail.com>
2023-07-20 12:23:53 -0600

    Implementacion de url base como argumento

--- src/components/utils/fetch.js
+++ src/components/utils/fetch.js
@@ -1,15 +1,13 @@
 export const FetchData = async (url, authToken) => {
-    try {
-        const response = await fetch(url, {
-            headers: {
-                Authorization: authToken
-            }
-        }
-        );
-        const data = await response.json();
-        return data // Hacer algo con los datos obtenidos
-            ;
-    } catch (error) {
-        console.error('Error al realizar la solicitud:', error);
-    }
-}
+	try {
+		const response = await fetch(`http://localhost:8082/${url}`, {
+			headers: {
+				Authorization: authToken,
+			},
+		});
+		const data = await response.json();
+		return data; // Hacer algo con los datos obtenidos
+	} catch (error) {
+		console.error("Error al realizar la solicitud:", error);
+	}
+};

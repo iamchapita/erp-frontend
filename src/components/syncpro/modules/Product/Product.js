9e98ac3e13f23526ca46a859bb5ab56e23104918
MarionMBC <Marion.bustamante@unah.hn>
2023-07-29 01:18:57 -0600

    Se agregoó una nueva libreriía de MUI

--- src/components/syncpro/modules/Product/Product.js
+++ src/components/syncpro/modules/Product/Product.js
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react'
-import { useSelector } from 'react-redux'
-import { FetchData } from '../../../utils/fetch';
-import { Tab } from '../../../Tab';
-import { productTableHead, productTabs } from '../../../../data/util';
-import { Title } from '../../../Title';
-import { DataGrid } from '@mui/x-data-grid';
+import React, {useEffect, useState} from 'react'
+import {useSelector} from 'react-redux'
+import {FetchData} from '../../../utils/fetch';
+import {Tab} from '../../../Tab';
+import {productTableHead, productTabs} from '../../../../data/util';
+import {Title} from '../../../Title';
+import {DataGrid} from '@mui/x-data-grid';
 import InputComponent from '../../../InputComponent';
-
+import {Textarea} from '@mui/joy';
 
 export const Product = () => {
 
     const [info, setInfo] = useState()
-    const { accessToken } = useSelector(state => state.auth);
+    const {accessToken} = useSelector(state => state.auth);
 
 
     useEffect(() => {
-        FetchData('http://localhost:8082/product/getProduct/', accessToken)
+        FetchData('product/getProduct/', accessToken)
             .then(data => {
                 setInfo(data);
             });
@@ -23,35 +23,49 @@ export const Product = () => {
 
 
     return (
-        <div className='p-5 text-start'>
-            <Title title={'Productos'} />
-            <Tab data={productTabs} />
+        <div className='p-5 text-start w-full'>
+            <Title title={'Productos'}/>
+            <Tab data={productTabs}/>
             <div
                 className='grid grid-cols-1'
             >
                 {info && <DataGrid className='col-span-1'
 
-                    autoHeight
-                    density='compact'
-                    columns={productTableHead}
-                    rows={info}
-                    initialState={
-                        {
-                            pagination: {
-                                paginationModel: { page: 0, pageSize: 5 },
-                            },
-                        }
-                    }
-                    pageSizeOptions={[5, 10, 20]}
+                                   autoHeight
+                                   density='compact'
+                                   columns={productTableHead}
+                                   rows={info}
+                                   initialState={
+                                       {
+                                           pagination: {
+                                               paginationModel: {page: 0, pageSize: 5},
+                                           },
+                                       }
+                                   }
+                                   pageSizeOptions={[5, 10, 20]}
 
                 />}
             </div>
             <div className='my-2'>
-                <Title title={'Agregar producto'} />
-                <div className='grid grid-cols-1 p-5 rounded bg-red-400 sm:grid-cols-2'>
-                    <p>Detalles básicos</p>
-                    <InputComponent required={true} placeholder={'Nombre del producto'} />
+                <Title title={'Agregar producto'}/>
+                <div className='[&>*]:flex [&>*]:items-center p-5 rounded bg-custom-300 sm:grid-cols-2'>
+                    <div>
+                        <p>Detalles básicos: </p>
+                        <InputComponent required={true} placeholder={'Nombre del producto'}/>
+                    </div>
+                    <div>
+                        <p>Descripción: </p>
+                        <Textarea
+                            color="primary"
+                            disabled={false}
+                            minRows={2}
+                            placeholder="fggd"
+                            size="lg"
+                            variant="solid"
+                        />
+                    </div>
                 </div>
+
 
             </div>
         </div>

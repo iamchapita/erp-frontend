977b51a38d0b0334a6f5e536be600f84b0fc9a7d
iamchapita <alejandrom646@gmail.com>
2023-08-05 16:16:23 -0600

    Formateo y eliminación de console.log

--- src/reducers/productReducer.js
+++ src/reducers/productReducer.js
@@ -1,67 +1,63 @@
 import { types } from "../types/types";
 
 const initialState = {
-    products: [],
-    productCategories: [],
-    productUnities: [],
-    product: {
-        id: '',
-        productCode: '',
-        name: '',
-        description: '',
-        idProductCategoryFK: '',
-        idProductUnityFK: '',
-        taxablePrice: '',
-        taxExemptPrice: '',
-        salePrice: '',
-        images: '',
-        status: '',
-        elaborationDate: '',
-        expirationDate: ''
-    },
-}
-
-
-
+	products: [],
+	productCategories: [],
+	productUnities: [],
+	product: {
+		id: "",
+		productCode: "",
+		name: "",
+		description: "",
+		idProductCategoryFK: "",
+		idProductUnityFK: "",
+		taxablePrice: "",
+		taxExemptPrice: "",
+		salePrice: "",
+		images: "",
+		status: "",
+		elaborationDate: "",
+		expirationDate: "",
+	},
+};
 
 export const productReducer = (state = initialState, action) => {
-    switch (action?.type) {
-        case types.productActive:
-            return {
-                ...state,
-                product: {
-                    ...state.product,
-                    ...action.payload
-                }
-            }
-        case types.productLoaded:
-            return {
-                ...state,
-                products: [...action.payload]
-            }
-        case types.product:
-            return action.payload;
-        case types.productAdd:
-            return action.payload;
-        case types.productUpdate:
-            return action.payload;
-        case types.productDelete:
-            return action.payload;
-        case types.productClear:
-            return initialState;
-        case types.productCategoriesLoaded:
-            return {
-                ...state,
-                productCategories: action.payload
-            }
-        case types.productUnitiesLoaded:
-            return {
-                ...state,
-                productUnities: action.payload
-            }
+	switch (action?.type) {
+		case types.productActive:
+			return {
+				...state,
+				product: {
+					...state.product,
+					...action.payload,
+				},
+			};
+		case types.productLoaded:
+			return {
+				...state,
+				products: [...action.payload],
+			};
+		case types.product:
+			return action.payload;
+		case types.productAdd:
+			return action.payload;
+		case types.productUpdate:
+			return action.payload;
+		case types.productDelete:
+			return action.payload;
+		case types.productClear:
+			return initialState;
+		case types.productCategoriesLoaded:
+			return {
+				...state,
+				productCategories: action.payload,
+			};
+		case types.productUnitiesLoaded:
+			return {
+				...state,
+				productUnities: action.payload,
+			};
 
-        default:
-            return state;
-    }
-}
-
+		default:
+			return state;
+	}
+};

b21073185e376fd1d041dd5c52a427d7998dcd90
iamchapita <alejandrom646@gmail.com>
2023-08-07 01:38:00 -0600

    Implementación de acciones de guardado

--- src/actions/customer.action.js
+++ src/actions/customer.action.js
@@ -15,42 +15,86 @@ export const customersLoaded = (customers) => ({
 	payload: customers,
 });
 
-// export const uploadCustomer = (token) => {
-// 	return async (dispatch, getState) => {
-// 		const { customer } = getState().product;
-// 		try {
-// 			Swal.fire({
-// 				title: "Subiendo producto",
-// 				text: "Por favor espere...",
-// 				allowOutsideClick: false,
-// 				allowEnterKey: false,
-// 				didOpen: () => {
-// 					Swal.showLoading();
-// 				},
-// 			});
-// 		} catch (error) {
-// 			console.error(error);
-// 			Swal.fire({
-// 				icon: "error",
-// 				title: "Upload Failed",
-// 				text: "An error occurred during the upload.",
-// 			});
-// 		}
-// 		FetchData("product/addProduct", token, "POST", product).then((data) => {
-// 			console.log(product);
-// 			console.log(data);
-// 			dispatch(productActive(product));
-// 			Swal.close();
-// 			Swal.fire({
-// 				icon: "success",
-// 				title: "Producto creado",
-// 				text: "El producto se ha creado correctamente.",
-// 			});
-// 		});
-// 	};
-// };
+export const loadCustomerType = (token) => {
+	return async (dispatch) => {
+		FetchData("customer/getCustomerTypes", token).then((data) => {
+			dispatch(customerTypeLoaded(data));
+		});
+	};
+};
 
-// export const productActive = (product) => ({
-// 	type: types.productActive,
-// 	payload: product,
-// });
+export const customerTypeLoaded = (customerTypes) => ({
+	type: types.customerTypesLoaded,
+	payload: customerTypes,
+});
+
+export const uploadCustomer = (form, token) => {
+	return async (dispatch, getState) => {
+		const customer = {
+			idCustomerTypeFK: form.idCustomerTypeFK,
+			firstNames: form.firstNames,
+			lastNames: form.lastNames,
+			city: form.city,
+			country: form.country,
+			direction: form.direction,
+			// ContactInfo
+			phoneNumber: form.phoneNumber,
+			email: form.email,
+			// naturalCustomerTypeDetails
+			naturalRtn: form.naturalRtn,
+			// businessCustomerTypeDetails
+			businessName: form.businessName,
+			businessRtn: form.businessRtn,
+			hasCredit: form.hasCredit,
+			creditAmount: form.creditAmount,
+		};
+
+		// Pasando a null las propiedades vacías
+		for (const [prop, value] of Object.entries(customer)) {
+			customer[prop] = value === "" ? null : value;
+		}
+
+		try {
+			Swal.fire({
+				title: "Guardando cliente",
+				text: "Por favor espere...",
+				allowOutsideClick: false,
+				allowEnterKey: false,
+				didOpen: () => {
+					Swal.showLoading();
+				},
+			});
+		} catch (error) {
+			console.error(error);
+			Swal.fire({
+				icon: "error",
+				title: "Fallo en Guardado",
+				text: `Ocurrió un error mientras de guardaba.`,
+			});
+		}
+
+		FetchData("Customer/addCustomer", token, "POST", customer)
+			.then((data) => {
+				// console.log(data);
+				dispatch(customerActive(customer));
+				Swal.close();
+				Swal.fire({
+					icon: "success",
+					title: "Producto creado",
+					text: "El producto se ha creado correctamente.",
+				});
+			})
+			.catch((error) => {
+				Swal.fire({
+					icon: "error",
+					title: "Fallo en Guardado",
+					text: `Ocurrió un error mientras de guardaba.`,
+				});
+			});
+	};
+};
+
+export const customerActive = (customer) => ({
+	type: types.customerActive,
+	payload: customer,
+});

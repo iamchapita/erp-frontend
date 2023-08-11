bdb51f5b6f61417c6a4ba87ea523a9d8cd6020ef
iamchapita <alejandrom646@gmail.com>
2023-08-10 23:55:37 -0600

    Implementación incompleta de actualización

--- src/components/syncpro/modules/Customer/Customer.js
+++ src/components/syncpro/modules/Customer/Customer.js
@@ -10,16 +10,15 @@ import {
 import { Title } from "../../../Title";
 import { DataGrid } from "@mui/x-data-grid";
 import InputComponent from "../../../InputComponent";
-import { AutocompleteComponent } from "../../../Autocomplete";
 import { useForm } from "../../../../hooks/useForm";
 import {
 	uploadCustomer,
 	loadCustomers,
-	loadCustomerType,
 	loadBusinessCustomers,
 	loadNaturalCustomers,
+	loadCustomerById,
 } from "../../../../actions/customer.action";
-import PeopleIcon from "@mui/icons-material/People";
+import { cleanFormsFields } from "../../../../data/cleanFormsFields";
 
 export const Customer = React.memo(() => {
 	const dispatch = useDispatch();
@@ -37,6 +36,7 @@ export const Customer = React.memo(() => {
 	const customerTypes = useSelector((state) => state.customer.customerTypes);
 
 	const [activeTab, setActiveTab] = useState(0);
+	const [selectedRow, setSelectedRow] = useState(null);
 
 	useEffect(() => {
 		accessToken && dispatch(loadCustomers(accessToken));
@@ -62,10 +62,69 @@ export const Customer = React.memo(() => {
 			}
 		}
 
-		dispatch(uploadCustomer(formState, accessToken));
+		if (selectedRow !== null) {
+			// dispatch(updateCustomer(formState, accessToken));
+		} else {
+			dispatch(uploadCustomer(formState, accessToken));
+		}
 	};
 
-	const [formState, handleInputChange, , handleSubmit] = useForm({
+	useEffect(() => {
+		if (selectedRow !== null) {
+
+			// Estableciendo a vacio los campos que son null
+			const ca = cleanFormsFields(customerActive[0]);
+
+			setFormState({
+				idCustomerTypeFK: ca.idCustomerTypeFK,
+				firstNames: ca.firstNames,
+				lastNames: ca.lastNames,
+				country: ca.country,
+				city: ca.city,
+				direction: ca.direction,
+				phoneNumber: ca.phoneNumber,
+				email: ca.email,
+				businessName: ca.businessName,
+				businessRtn: ca.businessRtn,
+				hasCredit: ca.hasCredit,
+				creditAmount: ca.creditAmount,
+				naturalRtn: ca.naturalRtn,
+			});
+		}
+	}, [customerActive]);
+
+	const handleRowClick = (params) => {
+		setSelectedRow(params.row);
+		dispatch(loadCustomerById(params.row.id, accessToken));
+	};
+
+	const handleKeyDown = () => {
+		setSelectedRow(null);
+		setFormState({
+			idCustomerTypeFK: "",
+			firstNames: "",
+			lastNames: "",
+			country: "",
+			city: "",
+			direction: "",
+			phoneNumber: "",
+			email: "",
+			businessName: "",
+			businessRtn: "",
+			hasCredit: "",
+			creditAmount: "",
+			naturalRtn: "",
+		});
+	};
+
+	const [
+		formState,
+		handleInputChange,
+		handleCheck,
+		handleSubmit,
+		setFormState,
+		reset,
+	] = useForm({
 		// Customer
 		idCustomerTypeFK: "",
 		firstNames: "",
@@ -85,16 +144,6 @@ export const Customer = React.memo(() => {
 		naturalRtn: "",
 	});
 
-	const autoCompleteCustomerType = {
-		name: "idCustomerTypeFK",
-		handleInputChange,
-		dispatchProp: loadCustomerType,
-		items: customerTypes,
-		optionName: "name",
-		required: true,
-		icon: <PeopleIcon className="text-custom-300" />,
-	};
-
 	return (
 		<div className="p-5 text-start w-full">
 			<Title title={"Clientes"} />
@@ -113,6 +162,9 @@ export const Customer = React.memo(() => {
 								paginationModel: { page: 0, pageSize: 5 },
 							},
 						}}
+						onRowDoubleClick={handleRowClick}
+						selectedRow={selectedRow}
+						onCellKeyDown={handleKeyDown}
 						pageSizeOptions={[5, 10, 20]}
 					/>
 				)}
@@ -129,6 +181,9 @@ export const Customer = React.memo(() => {
 								paginationModel: { page: 0, pageSize: 5 },
 							},
 						}}
+						onRowDoubleClick={handleRowClick}
+						selectedRow={selectedRow}
+						onCellKeyDown={handleKeyDown}
 						pageSizeOptions={[5, 10, 20]}
 					/>
 				)}
@@ -145,13 +200,20 @@ export const Customer = React.memo(() => {
 								paginationModel: { page: 0, pageSize: 5 },
 							},
 						}}
+						onRowDoubleClick={handleRowClick}
+						selectedRow={selectedRow}
+						onCellKeyDown={handleKeyDown}
 						pageSizeOptions={[5, 10, 20]}
 					/>
 				)}
 			</div>
 
 			<form onSubmit={handlePost} className="my-2">
-				<Title title={"Agregar Cliente"} />
+				{selectedRow === null ? (
+					<Title title={"Agregar Cliente"} />
+				) : (
+					<Title title={"Actualizar Cliente"} />
+				)}
 				<div className="[&>*]:my-2 [&>*]:md:m-2 [&>*]:[&>*]:mb-2 grid grid-cols-1 md:grid-cols-2 [&>*]:items-center px-5 rounded bg-white sm:grid-cols-2">
 					<div className="">
 						<p className="text-custom-150 font-normal">
@@ -163,6 +225,7 @@ export const Customer = React.memo(() => {
 							className={"w-full"}
 							required={true}
 							placeholder={"Juan Antonio"}
+							value={formState.firstNames}
 						/>
 					</div>
 
@@ -176,6 +239,7 @@ export const Customer = React.memo(() => {
 							className={"w-full"}
 							required={true}
 							placeholder={"Pérez Molina"}
+							value={formState.lastNames}
 						/>
 					</div>
 
@@ -187,6 +251,7 @@ export const Customer = React.memo(() => {
 							className={"w-full"}
 							required={true}
 							placeholder={"Honduras"}
+							value={formState.country}
 						/>
 					</div>
 
@@ -198,6 +263,7 @@ export const Customer = React.memo(() => {
 							className={"w-full"}
 							required={true}
 							placeholder={"Distrito Central"}
+							value={formState.city}
 						/>
 					</div>
 
@@ -242,6 +308,7 @@ export const Customer = React.memo(() => {
 							className={"w-full"}
 							name={"phoneNumber"}
 							placeholder={"33959171"}
+							value={formState.phoneNumber}
 						/>
 					</div>
 
@@ -253,8 +320,8 @@ export const Customer = React.memo(() => {
 							handleInputChange={handleInputChange}
 							className={"w-full"}
 							name={"email"}
-							// type={'email'}
 							placeholder={"correo@ejemplo.com"}
+							value={formState.email}
 						/>
 					</div>
 
@@ -262,10 +329,60 @@ export const Customer = React.memo(() => {
 						<p className="text-custom-150 font-normal">
 							Tipo de Cliente:{" "}
 						</p>
-						<AutocompleteComponent
-							{...autoCompleteCustomerType}
-							handleInputChange={handleInputChange}
-						/>
+						<select
+							className="w-full
+								h-10
+								p-2
+								rounded
+								placeholder:text-custom-100
+								bg-inherit
+								border-custom-100
+								focus:outline-none
+								ring-0
+								focus:ring-0
+								outline-none
+								focus:border-custom-400
+							"
+							name="idCustomerTypeFK"
+							value={formState.idCustomerTypeFK}
+							onChange={handleInputChange}
+							required={true}
+						>
+							<option
+								className="
+									text-custom-100
+									bg-custom-300
+									hover:bg-custom-250
+									active:bg-custom-200
+								"
+								value={0}
+								defaultChecked={true}
+							>
+								Seleccione un tipo
+							</option>
+							<option
+								className="
+									text-custom-100
+									bg-custom-300
+									hover:bg-custom-250
+									active:bg-custom-200
+								"
+								value={1}
+							>
+								Natural
+							</option>
+							<option
+								className="
+									text-custom-100
+									bg-custom-300
+									hover:bg-custom-250
+									active:bg-custom-200
+								"
+								value={2}
+							>
+								Empresarial
+							</option>
+						</select>
 					</div>
 
 					{formState.idCustomerTypeFK === 1 ? (
@@ -279,6 +396,7 @@ export const Customer = React.memo(() => {
 								placeholder={
 									"14 caracteres máximo, sin guiones"
 								}
+								value={formState.naturalRtn}
 							/>
 						</div>
 					) : null}
@@ -295,6 +413,7 @@ export const Customer = React.memo(() => {
 									name={"businessName"}
 									required={true}
 									placeholder={"Empresa SA"}
+									value={formState.businessName}
 								/>
 							</div>
 
@@ -311,6 +430,7 @@ export const Customer = React.memo(() => {
 									placeholder={
 										"14 caracteres máximo, sin guiones"
 									}
+									value={formState.businessRtn}
 								/>
 							</div>
 
@@ -375,6 +495,7 @@ export const Customer = React.memo(() => {
 											name={"creditAmount"}
 											required={true}
 											placeholder={"10000"}
+											value={formState.creditAmount}
 										/>
 									</div>
 								) : null}
@@ -385,23 +506,25 @@ export const Customer = React.memo(() => {
 						<button
 							type="submit"
 							className="
-							w-full
-							h-10
-							p-2
-							rounded
-							text-custom-100
-							bg-custom-300
-							hover:bg-custom-250
-							active:bg-custom-200
-							focus:outline-none
-							ring-0
-							focus:ring-0
-							outline-none
-							focus:border-custom-400
-							font-semibold
+								w-full
+								h-10
+								p-2
+								rounded
+								text-custom-100
+								bg-custom-300
+								hover:bg-custom-250
+								active:bg-custom-200
+								focus:outline-none
+								ring-0
+								focus:ring-0
+								outline-none
+								focus:border-custom-400
+								font-semibold
 							"
 						>
-							Agregar cliente
+							{selectedRow === null
+								? "Agregar Cliente"
+								: "Actualizar Cliente"}
 						</button>
 					</div>
 				</div>

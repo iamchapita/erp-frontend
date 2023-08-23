7c8ffc53a7366d548ba581565aa4df463f6a4eb6
MarionMBC <Marion.bustamante@unah.hn>
2023-08-22 19:06:11 -0600

    SW

--- src/App.js
+++ src/App.js
@@ -3,9 +3,33 @@ import { store } from "./store/store";
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./routes/AppRouter";
 import Footer from "./components/syncpro/Dashboard/Graph";
-import React from "react";
+import React, { useEffect } from "react";
+import { useServiceWorker } from "./serviceWorkerUpdates";
+import addNotification from "react-push-notification";
 
 const App = () => {
+
+	const { waitingWorker, showReload, reloadPage } = useServiceWorker();
+
+
+	useEffect(() => {
+		if (showReload && waitingWorker) {
+		  addNotification({
+			title: "Actualización disponible",
+			subtitle: "Actualización disponible",
+			message: "Presione para actualizar",
+			theme: "darkblue",
+			native: true, // when using native, your OS will handle theming.
+			duration: 5000, // when using native, the notification will close after the `duration` ms.
+			backgroundColor: "#000",
+			theme: "darkblue",
+			onClick: () => {
+							reloadPage();
+			},
+					  });
+		}
+	  }, [waitingWorker, showReload, reloadPage]);
+
 	return (
 		<Provider store={store}>
 			<BrowserRouter>

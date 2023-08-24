956ce4d6f1e12b90b76fd063ec190babf0148b45
MarionMBC <Marion.bustamante@unah.hn>
2023-08-24 02:35:25 -0600

    Ventas

--- src/actions/sales.action.js
+++ src/actions/sales.action.js
@@ -1,6 +1,7 @@
 import { FetchData } from "../components/utils/fetch";
 import { types } from "../types/types";
 import { getTransactType } from "../components/utils/getTransactType";
+import Swal from "sweetalert2";
 
 const module = "Ventas";
 
@@ -35,13 +36,36 @@ export const uploadpurchaseOrderAction = (table, transactInfo, token) => {
 		table.idTransactType = getTransactType(transactInfo);
 		table.status === "1" ? (table.status = 1) : (table.status = 0);
 		
-		FetchData("sales/addPurchaseOrder", token, "POST", table).then(
-			(data) => {
-				console.log('data', data);
-				dispatch(purchaseOrderActive(data));
-				dispatch(loadPurchaseOrder(token));
-
+		Swal.fire({
+			title: "¿Está seguro?",
+			text: "Está a punto de crear una nueva orden de compra",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			cancelButtonText: "Cancelar",
+			confirmButtonText: "Sí, crear",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				FetchData("sales/addPurchaseOrder", token, "POST", table).then(
+					(data) => {
+						console.log(data);
+						if (data.affectedRows === 1) {
+							Swal.fire(
+								"Orden de compra creada",
+								"La orden de compra ha sido creada con éxito",
+								"success"
+							);
+							dispatch(purchaseOrderActive(data));
+							dispatch(loadPurchaseOrder(token));
+						} else {
+							Swal.fire("Error", data.msg, "error");
+						}
+					}
+				);
 			}
-		);
+		});
 	};
 };
+
+
